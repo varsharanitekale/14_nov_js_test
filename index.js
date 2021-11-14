a73cf8c3e5bcd0541a5656f8593a2271a3d450ff
varsharanitekale <varsharanitekale50@gmail.com>
2021-11-14 20:32:22 +0530

    first initial

---
+++ index.js
@@ -0,0 +1,19 @@
+function fun1(){
+     var  myvar = document.getElementById("ans");
+     myvar.innerText="Yes, I accept all the major credit card's";
+     myvar.style.color="green";
+}
+function fun2(){
+    var myvar1=document.getElementById("ans");
+    myvar1.innerText="yes, i support local farmer's";
+    myvar1.style.color="green";
+
+
+}
+function fun3(){
+    var myvar2=document.getElementById("ans");
+    myvar2.innerText="yes we use organic ingredient";
+    myvar2.style.color="green";
+
+
+}
